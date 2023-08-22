77d93d79bc2c2a3ec9f1c15af9fbf499cfde1920
Jiameng Shi <jiameng@uga.edu>
2023-08-22 16:46:03 -0400

    Add ENABLE_AFL macro

--- compiler-plugins/IPEA-San/IPEASan.h
+++ compiler-plugins/IPEA-San/IPEASan.h
@@ -7,6 +7,13 @@
 #define AFL_R(x)                    (random() % (x))
 
 #define WHITE_LIST_OR_BLACK_LIST    0 // 1 for white and 0 for black
-#define ENABLE_LOOP_OPTIMIZATION    1
+
+#ifndef ENABLE_LOOP_OPTIMIZATION
+  #define ENABLE_LOOP_OPTIMIZATION    1
+#endif
+
+#ifndef ENABLE_AFL
+  #define ENABLE_AFL                  1
+#endif
 
 #endif
