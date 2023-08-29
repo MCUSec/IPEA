37554d8db568aeaf21204def3c4050c75df66273
Jiameng Shi <jiameng@uga.edu>
2023-08-29 01:33:24 -0400

    Fix references

--- include/target/freertos/hooks.h
+++ include/target/freertos/hooks.h
@@ -13,17 +13,17 @@
 
 #include <stdint.h>
 
-extern void __usan_trace_new_thread_on_success(uint32_t thread_id);
+extern void __ipeasan_trace_new_thread_on_success(uint32_t thread_id);
 
-extern void __usan_trace_new_thread_on_fail(uint32_t thread_id);
+extern void __ipeasan_trace_new_thread_on_fail(uint32_t thread_id);
 
-extern void __usan_trace_switch_context(uint32_t thread_id);
+extern void __ipeasan_trace_switch_context(uint32_t thread_id);
 
-#define traceTASK_CREATE( xTask ) __usan_trace_new_thread_on_success((uint32_t)xTask)
+#define traceTASK_CREATE( xTask ) __ipeasan_trace_new_thread_on_success((uint32_t)xTask)
 
-#define traceTASK_CREATE_FAILED( pxNewTCB ) __usan_trace_new_thread_on_fail()
+#define traceTASK_CREATE_FAILED( pxNewTCB ) __ipeasan_trace_new_thread_on_fail()
 
-#define traceTASK_SWITCHED_IN() __usan_trace_switch_context((uint32_t)pxCurrentTCB)
+#define traceTASK_SWITCHED_IN() __ipeasan_trace_switch_context((uint32_t)pxCurrentTCB)
 
 
 #endif /* __FREERTOS_TRACER_H__ */
